2858ae41f53a57d9788225bd5f0dd2d880ff38ed
kylfulto <97705227+kylfulto@users.noreply.github.com>
2022-02-01 16:28:35 -0500

    Complete Exercise

---
+++ Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+
+var velocity = Vector2.ZERO
+
+var rotation_speed = 5.0
+var speed = 0.1
+var max_speed = 10.00
+
+func _ready():
+	pass 
+
+# warning-ignore:unused_argument
+func _physics_process(_delta):
+	position = position + velocity
+	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
+	
+# warning-ignore:standalone_expression
+	$exhaust.hide
+	
+	if Input.is_action_pressed("forward"):
+		velocity = velocity + Vector2(0,-speed).rotated(rotation)
+# warning-ignore:standalone_expression
+		$exhaust.show
+		
+	
+	if Input.is_action_pressed("left"):
+		rotation_degrees = rotation_degrees - rotation_speed
+	if Input.is_action_pressed("right"):
+		rotation_degrees = rotation_degrees + rotation_speed
+		
+	position.x = wrapf(position.x, 0, 1024)	
+	position.y = wrapf(position.y, 0, 600)
+	
